75d0185b5f899055dd0289f0eac427b9b68914f2
Tekendra Bhandari <tekendraa67@gmail.com>
2025-05-27 17:26:20 +0545

    add .cpp

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main(){
+    int num1, num2, sum;
+    cout << "Enter the first number: \n";
+    cin>> num1;
+    cout << "Enter the second number: \n";
+    cin>> num2;
+    cout <<"THe sum is :\n" << num1 + num2 ;
+    return 0;
+}
