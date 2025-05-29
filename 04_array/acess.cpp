6cb07330fa9746509fdf678f181ff8ac80504920
Tekendra Bhandari <tekendraa67@gmail.com>
2025-05-29 08:16:33 +0545

    vbsdva

---
+++ 04_array/acess.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int i, a[] = {1,2,3,4,5,6,7,8,9,10,11};
+    do {
+        cout<<"The arrays are: " << a[i] <<endl;
+        i++;
+    } while(i<11);
+
+    return 0;
+}
