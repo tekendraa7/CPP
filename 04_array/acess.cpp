16c3755b51337ec412afb07c7eef4217d6603864
Tekendra Bhandari <tekendraa67@gmail.com>
2025-05-29 08:18:11 +0545

    access array

--- 04_array/acess.cpp
+++ 04_array/acess.cpp
@@ -2,11 +2,10 @@
 using namespace std;
 int main() {
     // Write C++ code here
-    int i, a[] = {1,2,3,4,5,6,7,8,9,10,11};
+    int i=0, a[] = {1,2,3,4,5,6,7,8,9,10,11};
     do {
         cout<<"The arrays are: " << a[i] <<endl;
         i++;
     } while(i<11);
-
     return 0;
 }
